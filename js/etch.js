8753ef4efbb228cc6c8168707e2c6b35a8b4347b
Felipe Adrian Lopez <pon.felipon@gmail.com>
2015-07-23 14:39:44 -0700

    Estos son los archivos modificados para gregar el boton a la pagina

--- js/etch.js
+++ js/etch.js
@@ -1,8 +1,21 @@
 $(document).ready(function(){
 
+    var d= 16;
+    var ancho = d * 27;
+    $('.container').css('width', ancho);
+    tablero(d);
+
+})
+
+function limpiar(){
+    $('.grid').css('background-color', "#FFF");
+}
+
+function tablero(d){
+
     var grid = "<div class='grid'></div>"
-    for (var rows = 0; rows < 5; rows++){
-        for(var cols = 0; cols < 5; cols++){
+    for (var rows = 0; rows < d; rows++){
+        for(var cols = 0; cols < d; cols++){
             $('.container').append(grid);
         }
     }
@@ -11,4 +24,6 @@ $(document).ready(function(){
         $(this).css('background-color', "#000");
     },function(){ $(this).css('background-color', "#DDD");
         });
-})
+
+}
+
